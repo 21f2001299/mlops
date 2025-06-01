fb9d5dc7e42631eade53b2f6e733a49950dd5d84
Shibasis mukherjee <109910204+21f2001299@users.noreply.github.com>
2025-06-01 18:29:20 +0530

    Update train.py

--- dvc-versioning/train.py
+++ dvc-versioning/train.py
@@ -9,10 +9,9 @@ from sklearn import metrics
 import joblib
 
 
-pathname = os.path.dirname(sys.argv[0])
-path = os.path.abspath(pathname)
 
-data = pd.read_csv(os.path.join('data', 'iris.csv'))
+
+data = pd.read_csv('iris.csv')
 train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
 X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
 y_train = train.species
@@ -24,4 +23,4 @@ mod_dt.fit(X_train,y_train)
 prediction=mod_dt.predict(X_test)
 print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
 
-joblib.dump(mod_dt, "artifacts/model.joblib")
+joblib.dump(mod_dt, "model.joblib")
